658f20583b68a65eb914c3a7672c03df44c47827
onatvaris <onatvaris@gmail.com>
2023-05-29 21:01:48 +0300

    package manager configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,51 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-use-before-define': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'arrow-body-style': ['error', 'as-needed'],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-quotes': ['error', 'prefer-single'],
+    'operator-linebreak': ['error', 'before'],
+    'object-curly-newline': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-one-expression-per-line': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  }
 };
